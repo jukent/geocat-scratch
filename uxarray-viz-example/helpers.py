b05a1ec617f1f4e0259ca8a2be797414afa95a79
Philip Chmielowiec <67855069+philipc2@users.noreply.github.com>
2023-01-21 00:08:38 -0600

    helper functions & notebook setup

---
+++ uxarray-viz-example/helpers.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def ugrid_to_polygons():
+    pass
+
+
+def close_polygons():
+    pass
+
+
+def find_antimeridian_faces():
+    pass
+
+
+def split_antimeridian_faces():
+    pass
